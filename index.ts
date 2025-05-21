617e8e6e9f6e2477fa4331a33cc969abbf322021
bricked <git@bricked.dev>
2025-05-21 21:52:33 +0200

    feat: add compatibility for nes and snes

--- index.ts
+++ index.ts
@@ -37,7 +37,7 @@ const authorization = buildAuthorization({ username, webApiKey });
 
 const consoleIds = await getConsoleIds(authorization);
 const consoleId = consoleIds
-  .filter((v) => consoleName.includes(v.name))
+  .filter((v) => consoleName.toLowerCase().includes(v.name.replace(/\/.*Famicom/, "").toLowerCase()))
   .sort((a, b) => b.name.length - a.name.length)[0]?.id;
 if (consoleId === undefined) {
   throw new Error(
@@ -87,8 +87,37 @@ async function matchFile({
   }
 }
 
+// https://docs.retroachievements.org/developer-docs/game-identification.html
+async function createHash(data: ArrayBufferLike) {
+  if (consoleId === ConsoleId.NES) {
+    const header = new Uint8Array(data.slice(0, 0x10));
+    if (new TextDecoder().decode(header).startsWith("NES\u001a")) {
+      data = data.slice(0x10);
+    }
+  } else if (consoleId === ConsoleId.SNES) {
+    if (data.byteLength % 0x2000 === 0x200) {
+      data = data.slice(0x200);
+    }
+  } /* else if (consoleId === ConsoleId.NDS) {
+    const headerBuffer = data.slice(0, 0x160);
+    const header = new Uint8Array(headerBuffer);
+    const infoStart = header.at(0x68)!;
+    const info = data.slice(infoStart, infoStart + 0xA00);
+    const arm9Start = header.at(0x20)!;
+    const arm9Size = header.at(0x2C)!;
+    const arm9 = data.slice(arm9Start, arm9Start + arm9Size);
+    const arm7Start = header.at(0x30)!;
+    const arm7Size = header.at(0x3C)!;
+    const arm7 = data.slice(arm7Start, arm7Start + arm7Size);
+    data = await new Blob([headerBuffer, arm9, arm7, info]).arrayBuffer();
+  } */
+
+  let hash = new Bun.CryptoHasher("md5").update(data).digest().toHex();
+  return hash;
+}
+
 async function matchHash(data: ArrayBufferLike) {
-  const hash = new Bun.CryptoHasher("md5").update(data).digest().toHex();
+  const hash = await createHash(data);
   let game = games.find((v) => v.hashes?.some((v) => v.toLowerCase() === hash));
   return game;
 }
@@ -101,4 +130,10 @@ function matchTitle(path: string) {
     (v) => v.title.replaceAll(/\W/g, "") === title.replaceAll(/\W/g, ""),
   );
   return game;
+}
+
+enum ConsoleId {
+  NES = 7,
+  SNES = 3,
+  NDS = 18,
 }
